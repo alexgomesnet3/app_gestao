7841c3bbe013e4b7923b1dbee8842eac2fbc32f7
alexgomesnet3 <alex.gomes.net3@gmail.com>
2022-05-28 20:55:37 -0300

    Implement firts changes in Routes files and Controllers

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    return view('welcome');
-});
+    return 'Seja bem vindo!';
+});*/
+
+Route::get('/', 'PrincipalController@principal');
+
+Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+
+Route::get('/contato', 'ContatoController@contato');
+
+
